622a93822772b293c9cde81e428cb6f52121ec33
jcortes <jacortesmahmud@gmail.com>
2017-08-24 09:37:53 -0500

    Compound class with adapter and decorator patterns

--- Compound.ts
+++ Compound.ts
@@ -6,138 +6,94 @@ interface Honkable {
   honk();
 }
 
-class Animal {
-  quackType: Quackable;
-  honkType: Honkable;
-
-  constructor(public name: string) {
-    this.name = name;
-  }
-
-  tryToQuack() {
-    return this.quackType.quack();
-  }
-
-  tryToHonk() {
-    return this.honkType.honk();
-  }
-
-  setQuackAbility(newQuackType: Quackable) {
-    this.quackType = newQuackType;
-  }
-
-  setHonkAbility(newHonkType: Honkable) {
-    this.honkType = newHonkType;
-  }
-}
-
-class ItQuacks implements Quackable {
-  quack() {
-    return "It Quacks";
-  }
-}
-
-class CantQuack implements Quackable {
-  quack() {
-    return "Can't Quack";
-  }
-}
-
-class ItHonks implements Honkable {
-  honk() {
-    return "It Honks";
-  }
-}
-
-class CantHonk implements Honkable {
-  honk() {
-    return "Can't Honk";
-  }
-}
-
-class MallardDuck extends Animal implements Quackable {
-  constructor() {
-    super("MallardDuck");
-    this.honkType = new CantHonk();
-    this.quackType = new ItQuacks();
-  }
+class MallardDuck implements Quackable {
 
   quack() {
     return "MallardDuck Quack";
   }
 }
 
-class RedheadDuck extends Animal implements Quackable {
-  constructor() {
-    super("RedheadDuck");
-    this.honkType = new CantHonk();
-    this.quackType = new ItQuacks();
-  }
+class RedheadDuck implements Quackable {
 
   quack() {
     return "RedheadDuck Quack";
   }
 }
 
-class DuckCall extends Animal implements Quackable {
-  constructor() {
-    super("DuckCall");
-    this.honkType = new CantHonk();
-    this.quackType = new ItQuacks();
-  }
+class DuckCall implements Quackable {
 
   quack() {
     return "DuckCall Quack";
   }
 }
 
-class RubberDuck extends Animal implements Quackable {
-  constructor() {
-    super("RubberDuck");
-    this.honkType = new CantHonk();
-    this.quackType = new ItQuacks();
-  }
+class RubberDuck implements Quackable {
 
   quack() {
     return "RubberDuck Quack";
   }
 }
 
-class Goose extends Animal implements Honkable {
-  constructor() {
-    super("Goose");
-    this.honkType = new ItHonks();
-    this.quackType = new CantQuack();
-  }
+class Goose implements Honkable {
 
   honk() {
     return "Goose Honk";
   }
 }
 
-class DuckSimulator {
+class QuackCounter implements Quackable {
+  numberOfQuacks: number = 0;
+
+  constructor(public duck: Quackable) {
+    this.duck = duck;
+  }
+
+  quack() {
+    this.numberOfQuacks += 1;
+    return this.duck.quack();
+  }
+
+  getNumberOfQuacks() {
+    return this.numberOfQuacks;
+  }
+}
+
+class QuackToHonkAdapter implements Quackable {
+
+  constructor(public goose: Honkable) {
+    this.goose = goose;
+  }
+
+  quack() {
+    return this.goose.honk();
+  }
+}
+
+class AnimalSimulator {
 
   simulate(): void {
-    const mallardDuck: Animal = new MallardDuck();
-    const redheadDuck: Animal = new RedheadDuck();
-    const duckCall: Animal = new DuckCall();
-    const rubberDuck: Animal = new RubberDuck();
-    const goose: Animal = new Goose();
+    
+    const mallardDuck = new QuackCounter(new MallardDuck());
+    const redheadDuck = new RedheadDuck();
+    const duckCall = new DuckCall();
+    const rubberDuck = new RubberDuck();
+    const goose = new QuackToHonkAdapter(new Goose());
 
     this.runSimulate(mallardDuck);
     this.runSimulate(redheadDuck);
     this.runSimulate(duckCall);
     this.runSimulate(rubberDuck);
     this.runSimulate(goose);
+
+    console.log("mallardDuck", mallardDuck.getNumberOfQuacks());
   }
 
-  runSimulate(animal: Animal): void {
-    console.log("\nAnimal", animal.name);
-    console.log(animal.tryToHonk());
-    console.log(animal.tryToQuack());
+  runSimulate(duck: Quackable): void {
+    // console.log("\Duck", duck.name);
+    console.log(duck.quack());
   }
 }
 
-const duckSimulator = new DuckSimulator();
+const simulator = new AnimalSimulator();
+simulator.simulate();
 
-duckSimulator.simulate();
